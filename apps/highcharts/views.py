769cbc7a5c0bd65734379ec772367c8cb0b68c4f
thinkAmi <dev.thinkami@gmail.com>
2019-02-09 22:11:46 +0900

    RemovedInDjango30Warning: render_to_response() の対応
    メッセージ：
    RemovedInDjango30Warning: render_to_response() is deprecated in favor of render(). It has the same signature except that it also requires a request.
    
    対応：
    render_to_response() を render() に差し替え

--- apps/highcharts/views.py
+++ apps/highcharts/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render
 
 def total(request):
     '''品種別の合計数量'''
-    return render_to_response('total.jinja2')
+    return render(request, 'total.jinja2')
 
 
 def total_by_month(request):
     '''月別品種別の合計数量'''
-    return render_to_response('total_by_month.jinja2')
+    return render(request, 'total_by_month.jinja2')
