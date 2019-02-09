438731e9ba217c9d815bad2474204603df205cd8
thinkAmi <dev.thinkami@gmail.com>
2019-02-09 21:25:26 +0900

    whitenoise v4.0の破壊的変更に対応

--- dj_ringo_tabetter/settings.py
+++ dj_ringo_tabetter/settings.py
@@ -50,6 +50,7 @@ if 'DYNO' in os.environ:
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
