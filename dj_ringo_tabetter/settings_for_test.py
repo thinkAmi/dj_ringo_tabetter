91731c059e4590992559df72ec52e95a5faee058
thinkAmi <dev.thinkami+git@gmail.com>
2022-09-18 18:45:04 +0900

    settings.pyを分割

--- dj_ringo_tabetter/settings_for_test.py
+++
@@ -1,6 +0,0 @@
-from dj_ringo_tabetter.settings import *
-
-# whitenoise向けの設定はデフォルトに戻さないと、以下のエラーが発生する
-# ValueError: The file 'js/total_by_month.js' could not be found with
-# <whitenoise.django.GzipManifestStaticFilesStorage object at 0x112daa908>.
-STATICFILES_STORAGE = ''
--- dj_ringo_tabetter/wsgi.py
+++ dj_ringo_tabetter/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dj_ringo_tabetter.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dj_ringo_tabetter.settings.production")
 
 application = get_wsgi_application()
